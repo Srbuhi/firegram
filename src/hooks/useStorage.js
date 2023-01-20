37f6262e7e8e753e1312cff42b7e4852249bc54a
Srbuhi <srbuhi.sarg@gmail.com>
2023-01-20 19:58:04 +0400

    Add collection and addDoc

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,7 +1,8 @@
 import { async }                from '@firebase/util';
 import { useState , useEffect } from 'react';
-import { projectStorage }       from '../firebase/config';
+import { projectStorage, projectFirestore }  from '../firebase/config';
 import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { collection, addDoc } from 'firebase/firestore'; 
 
 const useStorage = (file) => {
     const [progress, setProgress] = useState(0);
@@ -24,10 +25,23 @@ const useStorage = (file) => {
           (err) => {
               setError(err);
           },
-          () => {
+            async () => {
               getDownloadURL(uploadTask.snapshot.ref).then((url) =>
                   setUrl(url)
               );
+            //   const createdAt = timestamp();
+              const createdAt = 123;
+            //  imagesCollectionRef.add({ url, createdAt })
+            try {
+                const docRef = await addDoc(collection(projectFirestore, "images"), {
+                  first: "Ada",
+                  last: "Lovelace",
+                  born: 1815
+                });
+                console.log("Document written with ID: ", docRef.id);
+              } catch (e) {
+                console.error("Error adding document: ", e);
+              }              
           }
       );
     }, [file])
