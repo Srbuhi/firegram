56f8f2015685b055d61c73ae711315ae4b58a03a
Srbuhi <srbuhi.sarg@gmail.com>
2023-01-22 23:06:21 +0400

    Dont show modal

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Title />
       <UploadForm />
       <ImageGrid setSelectedImg = {setSelectedImg} />
-      <Modal selectedImg = {selectedImg} />
+      { selectedImg && <Modal selectedImg = {selectedImg} /> }
     </div>
   );
 }
